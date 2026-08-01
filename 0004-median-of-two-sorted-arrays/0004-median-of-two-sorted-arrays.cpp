2e861220daa8c19e0d02acdbfa3bd79e5f23d8c6
Noctro <patelsakshya2@gmail.com>
2026-08-01 22:42:10 +0530

    Time: 0 ms (100%), Space: 95.2 MB (62.13%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-
-        int n = nums1.size();
-        int m = nums2.size();
-
-        int k = 0, j = 0;
-        vector<int> nums3;
-
-        while (k < n && j < m) {
-            if (nums1[k] <= nums2[j]) {
-                nums3.push_back(nums1[k]);
-                k++;
-            } else {
-                nums3.push_back(nums2[j]);
-                j++;
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int pos1 = -1, pos2 = -1;
+        if ((n1 + n2) % 2 == 0){
+            pos1 = (n1 + n2)/2 - 1;
+            pos2 = pos1 + 1;
+        } else {
+            pos1 = (n1 + n2 + 1)/2 - 1;
+            pos2 = pos1;
+        }
+        int curr = -1, i = -1, j = -1;
+        double val = 0.0;
+        double currVal = 0.0;
+        while (curr <= pos2){
+            curr += 1;
+            if (i+1 < n1 and j+1 < n2){
+                if (nums1[i+1] < nums2[j+1]){
+                    i+=1;
+                    currVal = nums1[i];
+                } else {
+                    j+=1;
+                    currVal = nums2[j];
+                }
+            } else if (i+1 < n1) {
+                    i+=1;
+                    currVal = nums1[i];
+            } else if (j+1 < n2) {
+                    j+=1;
+                    currVal = nums2[j];
+            }
+            
+            if (curr == pos1 or curr == pos2){
+                val += currVal;
             }
         }
-
-        while (k < n) {
-            nums3.push_back(nums1[k]);
-            k++;
+        if (pos2 == pos1){
+            return val;
         }
-
-        while (j < m) {
-            nums3.push_back(nums2[j]);
-            j++;
-        }
-
-        int sz = nums3.size();
-
-        if (sz % 2 != 0) {
-            return nums3[sz / 2];
-        }
-
-        return (nums3[sz / 2] + nums3[sz / 2 - 1]) / 2.0;
+        return val/2;
     }
 };
